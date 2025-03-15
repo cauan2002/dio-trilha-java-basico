742f304f4ab370ae03529dd3b2af6ec2355975de
cauan <cauanjardeson@gmail.com>
2025-03-15 13:28:01 -0300

    new commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
 
+
+        //declaração de variaveis
+        // NumeroDaConta, Agencia e Nome_cliente serão definidos pelo metodo "scanner.next()" e "scanner.nextInt()" da classe Scanner.
+
         int NumeroDaConta;
-        String Agencia = "067-8";
+        String Agencia ;
         String Nome_cliente;
         float Saldo = 254.54f;
 
+        //objeto da classe Scanner
         Scanner scanner = new Scanner(System.in);
 
+        //exibição de mensagem de inserção de dados e atribuição de variaveis pelo metodo scanner
         System.out.println("digite seu nome");
         Nome_cliente = scanner.next();
 
@@ -19,6 +25,8 @@ public class ContaTerminal {
         System.out.println("digite o numero da agencia");
         Agencia=scanner.next();
 
+
+        // exibição de mensagem final
         System.out.println("Olá ".concat(Nome_cliente)+ ", obrigado por criar uma conta em nosso banco, sua agência é: ".concat(Agencia)+ ", conta: " + NumeroDaConta + " e seu saldo" + Saldo + " já está disponível para saque");
 
 
